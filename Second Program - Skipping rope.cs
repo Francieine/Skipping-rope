2ad3f9e284430638b311c9f3ddb32854ecf40197
Francieine <73969123+Francieine@users.noreply.github.com>
2022-02-23 00:37:31 -0300

    Second Program in C sharp (C#)
    This is a simple code and you can calculate how many calories its burn skipping rope.
    I use concatanation and Parse in my code

---
+++ Second Program - Skipping rope.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Skipping_Rope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exemple how many calories I burn skippng rope//
+
+            int weight;
+            int time;
+            int intensity;
+            int results;
+
+
+            Console.WriteLine("\t\t\t===============Calc to the Calories for the skipping rope=================\r");
+
+            Console.WriteLine("\t\t\tType your weight in kg\r");
+            weight = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\t\t\tType your time of exercise\r");
+            time = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\t\t\tType your intensity in kg\r");
+            intensity = int.Parse(Console.ReadLine());
+
+            results = (weight + time + intensity) / 60;
+            Console.WriteLine("Congratulations! Your caloric expenditure is:" + results);
+
+
+            Console.ReadLine();
+        }
+    }
+}
